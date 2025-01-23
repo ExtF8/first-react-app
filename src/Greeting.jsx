e5030e73ea02d6f9fe04d95a7676ab0f44260a7a
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-01-23 19:18:48 +0200

    Removed es-lint ignore comment

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,11 +1,6 @@
 
 function Greeting(props) {
-    // eslint-disable-next-line react/prop-types
     return <h1>Hello, {props.name}</h1>
 }
-
-// Greeting.propTypes = {
-//     name: PropTypes.string.isRequired,
-// }
 
 export default Greeting
