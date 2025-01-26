578ec92996deecead31fcf70470b73271f576266
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-01-26 13:56:43 +0200

    Added code from state lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,56 @@
-// import { useState } from 'react';
-import Greeting from './Greeting.jsx';
-import Animals from './Animals.jsx';
-
+import { useState } from 'react';
 import './App.css';
 
+const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
+
 function App() {
-    const animals = ['Lion', 'Cow', 'Snake', 'Lizard'];
+    const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+    const [count, setCount] = useState(0)
+
+    const onButtonClick = color => () => {
+        setBackgroundColor(color);
+        setCount(count + 1)
+    };
 
     return (
-        <>
-            <Greeting name='User' />
-            <Animals animals={animals} />
-        </>
+        <div
+            className='App'
+            style={{
+                backgroundColor,
+            }}
+        >
+            <h1>Background color has been changed on the page {count} times</h1>
+            {COLORS.map(color => (
+                <button
+                    type='button'
+                    key={color}
+                    onClick={onButtonClick(color)}
+                    className={backgroundColor === color ? 'selected' : ''}
+                >
+                    {color}
+                </button>
+            ))}
+        </div>
     );
 }
 
 export default App;
+
+// // import { useState } from 'react';
+// import Greeting from './Greeting.jsx';
+// import Animals from './Animals.jsx';
+
+// import './App.css';
+
+// function App() {
+//     const animals = ['Lion', 'Cow', 'Snake', 'Lizard'];
+
+//     return (
+//         <>
+//             <Greeting name='User' />
+//             <Animals animals={animals} />
+//         </>
+//     );
+// }
+
+// export default App;
